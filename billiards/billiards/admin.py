6b29495316513ba5bf583cc586df744778a8c839
Mengxin Zhu <kane.mx@gmail.com>
2013-11-13 22:09:03 +0800

    Add more info to poolroom.

--- billiards/billiards/admin.py
+++ billiards/billiards/admin.py
@@ -5,20 +5,22 @@ Created on 2013年10月21日
 
 @author: kane
 '''
-from billiards.models import Poolroom, Match
+from billiards.models import Poolroom, Match, PoolroomEquipment
 from django.contrib import admin
 from bitfield import BitField
 from bitfield.forms import BitFieldCheckboxSelectMultiple
 from bitfield.admin import BitFieldListFilter
 
-class PoolroomAdmin(admin.ModelAdmin):
+class ModelWithFlagsAdmin(admin.ModelAdmin):
     formfield_overrides = {
-            BitField: {'widget': BitFieldCheckboxSelectMultiple},
+        BitField: {'widget': BitFieldCheckboxSelectMultiple},
     }
 
+class PoolroomAdmin(ModelWithFlagsAdmin):
     list_filter = (
             ('flags', BitFieldListFilter),
             )
 
 admin.site.register(Poolroom, PoolroomAdmin)
+admin.site.register(PoolroomEquipment)
 admin.site.register(Match)
