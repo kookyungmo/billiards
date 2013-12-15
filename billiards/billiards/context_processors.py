d3b85f476eadf93d9166613f22a09c938ef48c7b
Mengxin Zhu <kane.mx@gmail.com>
2013-12-15 17:06:07 +0800

    Add rev string on page's bottom.

--- billiards/billiards/context_processors.py
+++ billiards/billiards/context_processors.py
@@ -6,6 +6,7 @@ from billiards import settings
 import time
 
 BUILDID = 20131210
+REV = '2013.12.15.abcdef'
 def siteconf(request):
     """
     Adds ibilliards global context variables to the context.
@@ -13,4 +14,5 @@ def siteconf(request):
     """
     return {
             'buildid': int(time.time()) if settings.DEBUG else BUILDID,
+            'rev': REV
     }
