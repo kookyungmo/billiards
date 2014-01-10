d2874c8402184b2d92b91ce6723970074cb29897
Mengxin Zhu <kane.mx@gmail.com>
2014-01-10 21:20:11 +0800

    Add management pages for club's matches.
    1. add/modify matches
    2. view enroll of matches

--- billiards/billiards/views/exceptions.py
+++ billiards/billiards/views/exceptions.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render_to_response, get_object_or_404
+# -*- coding: utf-8 -*-
+# encoding: utf-8
+from django.shortcuts import render_to_response
 from billiards.settings import TEMPLATE_ROOT
 from django.template.context import RequestContext
 
@@ -7,4 +8,7 @@ def handler404(request):
     return render_to_response(TEMPLATE_ROOT + '404.html', context_instance=RequestContext(request))
 
 def handler500(request):
-    return render_to_response(TEMPLATE_ROOT + '500.html', context_instance=RequestContext(request))
+    return render_to_response(TEMPLATE_ROOT + '500.html', {'title': u'服务器休息一下', 'msg': u'一大波台球爱好者们正在访问此网站，服务器不堪重负，歇菜了，请稍候再访问'}, context_instance=RequestContext(request))
+
+def handler403(request):
+    return render_to_response(TEMPLATE_ROOT + '500.html', {'title': u'黑客来啦', 'msg': u'你没有访问该页面的权限'}, context_instance=RequestContext(request))
