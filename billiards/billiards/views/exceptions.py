b8fe638bbfd846328b044e2026f9fc5f432ee9dc
Mengxin Zhu <kane.mx@gmail.com>
2014-02-26 10:30:03 +0800

    Add GA events to track 403/404/500 error.

--- billiards/billiards/views/exceptions.py
+++ billiards/billiards/views/exceptions.py
@@ -8,7 +8,7 @@ def handler404(request):
     return render_to_response(TEMPLATE_ROOT + '404.html', context_instance=RequestContext(request))
 
 def handler500(request):
-    return render_to_response(TEMPLATE_ROOT + '500.html', {'title': u'服务器休息一下', 'msg': u'一大波台球爱好者们正在访问此网站，服务器不堪重负，歇菜了，请稍候再访问'}, context_instance=RequestContext(request))
+    return render_to_response(TEMPLATE_ROOT + '500.html', {'title': u'服务器休息一下', 'code':500, 'msg': u'一大波台球爱好者们正在访问此网站，服务器不堪重负，歇菜了，请稍候再访问'}, context_instance=RequestContext(request))
 
 def handler403(request):
-    return render_to_response(TEMPLATE_ROOT + '500.html', {'title': u'黑客来啦', 'msg': u'你没有访问该页面的权限'}, context_instance=RequestContext(request))
+    return render_to_response(TEMPLATE_ROOT + '500.html', {'title': u'黑客来啦', 'code':403, 'msg': u'你没有访问该页面的权限'}, context_instance=RequestContext(request))
