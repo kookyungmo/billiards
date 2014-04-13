197d5cdf54b6fd87bca94f2accaeb9b21f4094e1
Mengxin Zhu <kane.mx@gmail.com>
2014-04-13 14:29:13 +0800

    Imprvoe wechat sharing.

--- billiards/static/js/wechat.js
+++ billiards/static/js/wechat.js
@@ -4,31 +4,11 @@ function weixinShareTimeline(title,desc,link,imgUrl){
 
 function weixinShare(messageType, title, desc, link, imgUrl) {
 	if (typeof WeixinJSBridge != "undefined") {
-		WeixinApi.ready(function(Api) {
-			var wxData = {
-				"appId": "",
-				"img_url":imgUrl,
-				"link":link,
-				"desc":desc,
-				"title":title
-			};
-
-			var wxCallbacks = {
-				fail : function(resp) {
-					if (messageType == 'shareTimeline') {
-						showAjaxModal('点击右上角的按钮分享到朋友圈');
-					} else if (messageType == 'sendAppMessage'){
-						showAjaxModal('点击右上角的按钮分享给朋友');
-					}
-				},
-				confirm : function(resp) {
-				},
-			};
-			if (messageType == 'sendAppMessage')
-				Api.shareToFriend(wxData, wxCallbacks);
-			else if (messageType == 'shareTimeline')
-				Api.shareToTimeline(wxData, wxCallbacks);
-		});
+		if (messageType == 'shareTimeline') {
+			showAjaxModal('点击右上角的按钮分享到朋友圈');
+		} else if (messageType == 'sendAppMessage'){
+			showAjaxModal('点击右上角的按钮分享给朋友');
+		}
 	} else {
 		showAjaxModal('请先通过微信搜索 pktaiqiu 添加我为台球狂为好友，通过微信分享抢台费 :)');
 	}
