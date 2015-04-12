b1d2e68c89712e2a18e5b904fd686b5aabd35aa4
Mengxin Zhu <kane.mx@gmail.com>
2015-04-12 15:49:04 +0800

    Add analytics for mobile pages.

--- billiards/static/js/mobile/app.js
+++ billiards/static/js/mobile/app.js
@@ -136,7 +136,11 @@ function parseTime(timestr) {
 var ORDER_URL = "/transaction/goods/12345678901234567890123456789012";
 
 var staticurl = 'static/m/';
-angular.module('app',['ngRoute','hmTouchEvents','infinite-scroll'])
+angular.module('app',['ngRoute','hmTouchEvents','infinite-scroll','angulartics', 'angulartics.google.analytics'])
+.config(function ($analyticsProvider) {
+	$analyticsProvider.firstPageview(true); /* Records pages that don't use $state or $route */
+    $analyticsProvider.withAutoBase(true);  /* Records full path */
+})
 .service('scopeService', function() {
      return {
          safeApply: function ($scope, fn) {
