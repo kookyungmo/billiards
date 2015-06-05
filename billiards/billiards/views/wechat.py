44734c18a20ef8a98d94a7843c1936ac63326b21
bill <xuhao26@hotmail.com>
2015-06-05 22:51:37 +0800

    update june activity in wechat message

--- billiards/billiards/views/wechat.py
+++ billiards/billiards/views/wechat.py
@@ -38,7 +38,7 @@ from billiards.views.poolroom import getNearbyPoolrooms
 from django.contrib.auth.models import User
 from billiards.views.assistant import ASSISTANT_OFFER_FILTER, getAssistantOffers
 
-HELP_TITLE = u'【最好】五月'
+HELP_TITLE = u'只为更赞，疯狂一夏'
 MYORDER_KEYWORD = u'订单'
 
 def set_video():
@@ -330,7 +330,7 @@ class PKWechat(BaseRoBot):
                 LOGO_IMG_URL, 'http://bcs.duapp.com/billiardsalbum/2015/04/weixin4yue.jpg', 'http://eqxiu.com/s/CV3BCp3n')]
         
     def getHelpMesg(self):
-        return [(HELP_TITLE, u'【最好】五月', 'http://bj.bcebos.com/billiardsalbum/2015/05/weixin5yue.jpg', 'http://mp.weixin.qq.com/s?__biz=MzA5MzY0MTYxMw==&mid=205066960&idx=1&sn=cf73745980fe42018d8885ec29b05e2b#rd')]
+        return [(HELP_TITLE, u'只为更赞，疯狂一夏', 'http://bj.bcebos.com/billiardsalbum/2015/06/weixin6yue.jpg', 'http://mp.weixin.qq.com/s?__biz=MzA5MzY0MTYxMw==&mid=205942607&idx=1&sn=04be76db74642f7d28d0c240c38acf53#rd')]
     
     def help(self):
         def helpmessge(message):
